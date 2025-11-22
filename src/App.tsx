2af662fa73cde99c2660d69b1ea3d1608389372c
Matheus Silva <matheusmarques2604@gmail.com>
2025-11-22 01:22:15 -0300

    childrens de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading';
+
 function App(){
     console.log('Olá mundo')
     return (
         <div>
-        <h1>Olá mundo</h1>
+        <Heading>Olá mundo 2</Heading>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quia nobis est magni? Neque tempore provident voluptatem labore, inventore ipsa ipsam aliquid ratione nemo dicta natus recusandae animi cupiditate velit?</p>
         </div>
     )
