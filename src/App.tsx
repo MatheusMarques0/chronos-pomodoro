eaa83d246619528c5928c1eb5c8835e68605c42c
Matheus Silva <matheusmarques2604@gmail.com>
2026-03-28 01:57:44 -0300

    Estrutura de HTML e CSS da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 function App(){
     return (
-        <div>
-            <Heading> 
-                Olá mundo 1
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quia nobis est magni? Neque tempore provident voluptatem labore, inventore ipsa ipsam aliquid ratione nemo dicta natus recusandae animi cupiditate velit?</p>
-            
+        <>
+        <div className="container">
+            <div className="content">
+                <section>
+                   
+                </section>
+            </div>
         </div>
+        </>
     )
 }
 
