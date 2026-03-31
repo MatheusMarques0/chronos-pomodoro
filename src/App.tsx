f725065d1944e2926fec43bc74f9a52c372792bd
Matheus Silva <matheusmarques2604@gmail.com>
2026-03-30 23:38:39 -0300

    aula 25

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import './styles/global.css'
 import { Container } from './components/containter';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 function App(){
     return (
         <>
             <Container>
-               <Logo/>
+                <Logo/>
             </Container>
 
             <Container>
                 <Menu/>
+            </Container>
+
+            <Container>
+                <CountDown />
             </Container>
         </>
     )
