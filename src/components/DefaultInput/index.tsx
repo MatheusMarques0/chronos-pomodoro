b5f9f189184479c008f255236ef58f5cdf7061e4
Matheus Silva <matheusmarques2604@gmail.com>
2026-04-09 00:12:22 -0300

    desfazendo condicional

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,6 +1,6 @@
 type DefaultInputProps = {
     id: string;
-    labelText?: string;
+    labelText: string;
 } & React.ComponentProps<'input'>
 
 export function DefaultInput({ id
@@ -9,8 +9,7 @@ export function DefaultInput({ id
       ...rest }: DefaultInputProps){
     return(
         <>
-        {labelText && <label htmlFor={id}>{labelText}</label>} {/*Se tivesse mais de uma linha iria precisar usar ()*/ }
-        {/*Se tiver label text, faça isso:*/}
+        <label htmlFor={id}>{labelText}</label>
         <input id={id} type={type} {...rest} />
         </>
     )
