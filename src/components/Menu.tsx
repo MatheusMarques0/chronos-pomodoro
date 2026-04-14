f84c7c339395065d82ebc8e5df0db11eca832c8b
Matheus Silva <matheusmarques2604@gmail.com>
2026-04-13 22:54:02 -0300

    aula 36

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,28 @@
 import styles from './Menu.module.css';
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { useState } from 'react';
 
+type AvaiableThemes = 'dark' | 'light'
 
 export function Menu(){
+  const [theme, setTheme] = useState<AvaiableThemes>('dark')
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+    event.preventDefault()
+  }
+
    return <nav className={styles.menu}>
-    <a href="#" className={styles.menuLink}>
+    <h1>{theme}</h1>
+    <a href="#" className={styles.menuLink} aria-label='ir para a Home ' title='Ir para a home'>
       <HouseIcon/>
     </a>
-    <a href="#" className={styles.menuLink}>
+    <a href="#" className={styles.menuLink} aria-label='Ver histórico' title='Ver histórico'>
       <HistoryIcon/>
     </a>
-    <a href="#" className={styles.menuLink}>
+    <a href="#" className={styles.menuLink} aria-label='Configurações' title='Configurações'>
       <SettingsIcon/>
     </a>
-    <a href="#" className={styles.menuLink}>
+    <a href="#" className={styles.menuLink} aria-label='Mudar Tema' title='Mudar Tema' onClick={handleThemeChange}>
       <SunIcon/>
     </a>
    </nav> 
