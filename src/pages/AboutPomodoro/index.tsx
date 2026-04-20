aee922db70cf4ac95705a01e3e8757ceda9231ac
Matheus Silva <matheusmarques2604@gmail.com>
2026-04-20 00:08:25 -0300

    Aula 41

--- src/pages/AboutPomodoro/index.tsx
+++ src/pages/AboutPomodoro/index.tsx
@@ -18,7 +18,7 @@ export function AboutPomodoro() {
             garantir descansos para evitar o cansaço mental.
           </p>
 
-          <img src='https://placehold.co/1920x1080' alt='' />
+          <img src='https://placehold.co/1920x1080' alt='' width={600} height={400}/>
 
           <h2>Como funciona o Pomodoro tradicional?</h2>
           <ul>
