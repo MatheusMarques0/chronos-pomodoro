2900cf66fcc7314240851e9bf932c60c4473d76a
Matheus Silva <matheusmarques2604@gmail.com>
2026-07-27 02:45:37 -0300

    aula 42

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,18 @@
+import type { TaskModel } from "./TaskModel"
+
+// Estado --> Componente -- Props --> Filhos
+//Você sempre levra um estado do maior pro menor
+//Por enqunato, vamos colocar os nossos estados em um CONTEXTO, todos os componentes que usam esses estados podem usar esse contexto. Ele
+
+export type taskStateModel = {
+    tasks: TaskModel[]; // histórico, MainForm
+    secondsRemaining: number; //contador, histórico, MainForm, Button
+    formattedSecondesRemaining: string; // Título, CountDown
+    activeTask: TaskModel | null; //CountDown, Histórico, MainForm, Button
+    currentCycle: number; // Home
+    config: /* configuração do sistema em sim, MainForm*/{
+        workTime: number;
+        shortBreakTime: number;
+        longBreakTime: number;
+    }
+}
